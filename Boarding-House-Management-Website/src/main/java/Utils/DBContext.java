6e03add62b05f9abfd2d7d1882e02d8ba0d8ffb6
khaihvce190238 <khaihv.ce190238@gmail.com>
2026-02-25 07:34:40 +0700

    Add files via upload

---
+++ Boarding-House-Management-Website/src/main/java/Utils/DBContext.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+    public Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=;"
+                    + "user=sa; "
+                    + "password=HDanh;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=true;";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url);
+            System.out.println("✅ Connected to SQL Server successfully!");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Database connection failed: " + ex);
+        }
+    }
+}
